c5705ee326192940397f397415097728d3251b21
KoffeinFlummi <koffeinflummi@gmail.com>
2015-09-03 00:35:34 +0200

    Large options for iframe

--- slackin/__init__.py
+++ slackin/__init__.py
@@ -99,6 +99,7 @@ def iframe():
     active = [u for u in users if u["presence"] == "active"]
 
     return render_template("iframe.html",
+        largs="large" if "large" in request.args.keys() else "",
         users_active=len(active),
         users_total=len(users))
 
